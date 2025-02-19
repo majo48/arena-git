a175c5174a5934708f852af5b20286dc199e1ed5
majo48 <martin.jonasse@mail.ch>
2025-02-19 13:46:51 +0100

    Simplified some variable names for the sake of readability.

--- tutorials/testZeroBlob.py
+++ tutorials/testZeroBlob.py
@@ -15,18 +15,19 @@ from decouple import config
 
 # constants ========
 EDGE = 1200 # resolution of the Copernicus GLO-90 DSM
+FACTOR = 2.4
 
 # matrix ========
 
-def build_matrix(m_rows, m_cols):
+def build_matrix(rows, cols):
     """
     build matrix with random (non-zero) values
     :return: matrix: list
     """
     matrix = []
-    for i in range(m_rows):
+    for i in range(rows):
         col = bytearray([])  # mutable
-        for j in range(m_cols):
+        for j in range(cols):
             z = random.randint(1, 8849) # elevation in meters above mean sea level
             z2b = z.to_bytes(2, byteorder='big')
             col.append(z2b[0])  # high byte (big endian)
@@ -54,11 +55,11 @@ def build_db(connection):
     finally:
         return success
 
-def set_row(connection, srid, bytes, max_bytes):
+def set_row(connection, id_, bytes, max_bytes):
     """
     insert row into table rows
     :param connection: Connection
-    :param srid: integer
+    :param id_: integer
     :param bytes: bytearray
     :param max_bytes: integer
     :return: success: bool
@@ -67,9 +68,9 @@ def set_row(connection, srid, bytes, max_bytes):
     try:
         sql = 'INSERT INTO rows(id, len, row) VALUES(?, ?, zeroblob(?));'
         bytes_len = len(bytes)
-        connection.execute(sql, (srid, bytes_len, max_bytes))
+        connection.execute(sql, (id_, bytes_len, max_bytes))
         # write blob contents
-        with connection.blobopen("rows", "row", srid) as blob:
+        with connection.blobopen("rows", "row", id_) as blob:
             blob.write(bytes)
         connection.commit()
         success = True
@@ -79,16 +80,16 @@ def set_row(connection, srid, bytes, max_bytes):
     finally:
         return success
 
-def get_row(cursor, grid):
+def get_row(cursor, id_):
     """
     get one row from the database
     :param cursor: Cursor
-    :param grid: integer
+    :param id_: integer
     :return: row: tuple (id, length, bytearray), or None (error)
     """
     try:
         sql = "SELECT id, len, row FROM rows WHERE id=?;"
-        cursor.execute(sql, (grid,))
+        cursor.execute(sql, (id_,))
         local_row = cursor.fetchone()
         bytes_len = local_row[1]
         return local_row[0], local_row[1], local_row[2][:bytes_len] # id, length, bytearray
@@ -99,16 +100,13 @@ def get_row(cursor, grid):
 # main ========
 
 # build matrix
-factor = 2.4
 max_cols_in_row = 2*EDGE
-max_length = factor * max_cols_in_row # 2.4 * 2 * 1200 = 5760 bytes
-rows = EDGE
-cols = EDGE
-mtrx1 = build_matrix(rows, cols)
+max_length = FACTOR * max_cols_in_row # 2.4 * 2 * 1200 = 5760 bytes
+mtrx1 = build_matrix(EDGE, EDGE) # rows, cols
 # mtrx2 = build_matrix(rows, cols)
 
 try:
-    # build empty database wit a blob
+    # build empty database with a blob
     db_filename = config("DB_FILENAME")
     print("DB filename: "+db_filename)
     if os.path.exists(db_filename):
@@ -119,15 +117,15 @@ try:
     if not done:
         exit(1)
     # write first blob to database
-    idx = 0
-    done = set_row(conn, idx, mtrx1[idx], max_length)
+    index = 0
+    done = set_row(conn, index, mtrx1[index], max_length)
     if not done:
         exit(2)
     # read first blob
-    row = get_row(curs, idx)
-    if row is None:
+    test_row = get_row(curs, index)
+    if test_row is None:
         exit(3)
-    if mtrx1[idx] == row[2]:
+    if mtrx1[index] == test_row[2]:
         print("Match in bytearrays in matrix1 and database.")
     else:
         print("Mismatch in bytearrays")
