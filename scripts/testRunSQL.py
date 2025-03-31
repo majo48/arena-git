849c395911dea0fd2da0798fc17302a430d8adc6
majo48 <martin.jonasse@mail.ch>
2025-03-31 10:14:25 +0200

    Adapted the test run script to the upgrades bounding box with > 1 tiles.

--- scripts/testRunSQL.py
+++ scripts/testRunSQL.py
@@ -23,22 +23,25 @@ cityWohlen = (47.36173, 8.21417)        # (lat, long)
 citySeon = (47.34673, 8.16113)          # (lat, long)
 
 # main ========
-
-logging.debug('Begin unit test for database.')
-with Dbcache(config("DB_FILENAME")) as dbcache:
-    #
-    # create test route, waypoints and tracks
-    metadata = dbcache.dbsql.get_metadata()
-    route = Route(json.loads(metadata[1]))
-    waypoints = [ cityZug, cityBaar, cityCham ]
-    tracks = route.build_route('Zug-Baar-Cham', waypoints)
-    #
-    # test database elevation profile
-    print("Meters above sea level:")
-    for track in tracks["tracks"]:
-        # masl = cache.get_elevation(track[0], track[1]) # lat, long
-        # print(  'track: ('+f'{track[0]:.6f}'+', '+f'{track[1]:.6f}'+', elevation: '+str(masl)+')')
-        elevation = dbcache.get_flight_information( track[0], track[1])
-        print(  f'{track[0]:.6f}'+', '+f'{track[1]:.6f}'+', '+str(elevation))
-    pass
-logging.debug('End unit test for database.')
+try:
+    logging.debug('Begin unit test for database.')
+    with Dbcache(config("DB_FILENAME")) as dbcache:
+        #
+        # create test route, waypoints and tracks
+        metadata = dbcache.dbsql.get_arena_bounding_box()
+        route = Route(metadata)
+        waypoints = [ cityZug, cityBaar, cityCham ]
+        tracks = route.build_route('Zug-Baar-Cham', waypoints)
+        #
+        # test database elevation profile
+        print("Meters above sea level:")
+        for track in tracks["tracks"]:
+            elevation = dbcache.get_flight_information( track[0], track[1])
+            # print current coordinates, (cell elevation, next cell elevation, heading, compass)
+            print(  f'{track[0]:.6f}'+', '+f'{track[1]:.6f}'+', '+str(elevation))
+        pass
+    logging.debug('End unit test for database.')
+    exit(0)
+except Exception as err:
+    logging.error("Test route, unknown error: " + err.args)
+    exit(1)
